6fd5affcd75071076b843abaae0804aabf65cf98
Ela Kumer <elich@Elas-MacBook-Pro.local>
2022-10-31 17:20:04 +0100

    Entire project skeleton.

---
+++ IzvorniKod/frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import {format} from 'date-fns';
+import { useEffect, useState } from 'react';
+
+import './App.css';
+const baseUrl = "http://localhost:5000"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
