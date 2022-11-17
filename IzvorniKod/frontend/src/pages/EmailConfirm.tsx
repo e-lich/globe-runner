7b621ceefcff58bb943b3d332fab5909fd92beae
Ela Kumer <ek53440@fer.hr>
2022-11-17 02:07:38 +0100

    Email confirmation checking

---
+++ IzvorniKod/frontend/src/pages/EmailConfirm.tsx
@@ -0,0 +1,17 @@
+import { useNavigate, useNavigation } from "react-router-dom";
+
+function EmailConfirm() {
+
+    const navigator = useNavigate();
+
+    return (
+        <div className="d-flex justify-content-center m-4 Auth-form-container align-items-center">
+            <h1>Confirm your email</h1>
+            <button className="btn btn-primary" onClick={() => {
+                navigator("/signIn");
+            }}>Sign in</button>
+        </div>
+    );
+}
+
+export default EmailConfirm;
