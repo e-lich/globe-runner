822487f0b889134b0708f5844fdca202dea38e28
atom-cluster <vladimir.pezo@fer.hr>
2022-12-29 12:35:17 +0100

    did some styling to cartographer edit location popup

--- IzvorniKod/frontend/src/components/CartographerHomePopup.tsx
+++ IzvorniKod/frontend/src/components/CartographerHomePopup.tsx
@@ -1,4 +1,4 @@
-import { Container, TextField, InputAdornment, Button } from "@mui/material";
+import { Container, TextField, InputAdornment, Button, Paper } from "@mui/material";
 import { CSSProperties } from "react";
 type Props = {
   open: Boolean;
@@ -24,7 +24,7 @@ const CartographerHomePopup = ({ open, onClose }: Props) => {
 
   return (
     <div style={OVERLAY}>
-      <Container
+      <Paper
         sx={{
           backgroundColor: "white",
           padding: "20px 20px",
@@ -32,69 +32,113 @@ const CartographerHomePopup = ({ open, onClose }: Props) => {
           margin: "0 auto",
           justifyContent: "center",
           alignItems: "center",
+          width: "75%",
         }}
       >
       <div>{/*need to center this*/}
-        <TextField
-          label="Card title"
-          id="title"
-          sx={{ m: 1, width: '25ch' }}
-          type="text"
-          defaultValue={locationData.title}
-        />
-        <TextField
-          id="description"
-          label="Description"
-          placeholder="Please describe the location in couple of sentences"
-          multiline
-          sx={{ m: 1, width: '25ch' }}
-          defaultValue={locationData.description}
-        />
+        <div style={{
+          justifyContent: "center", 
+          alignItems: "center",
+          display: "flex",
+          flexWrap: "wrap",
+        } /*center content*/}>
+          <TextField
+            label="Card title"
+            id="title"
+            sx={{ m: 1, width: '25ch' }}
+            type="text"
+            defaultValue={locationData.title}
+          />
+          <TextField
+            id="description"
+            label="Description"
+            placeholder="Please describe the location in couple of sentences"
+            multiline
+            sx={{ m: 1, width: '25ch' }}
+            defaultValue={locationData.description}
+          />
+        </div>
+        
         <hr/>
-        <TextField
-          label="Location latitude"
-          id="lat"
-          sx={{ m: 1, width: '25ch' }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">°</InputAdornment>,
-            inputProps: {min: -90, max: 90}
-          }}
-          type="number"
-          defaultValue={locationData.latitude}
-        />
-        <TextField
-          label="Location longitude"
-          id="long"
-          sx={{ m: 1, width: '25ch' }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">°</InputAdornment>,
-            inputProps: {min: -180, max: 180}
-          }}
-          type="number"
-          defaultValue={locationData.longitude}
-        />
+        <div style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexWrap: "wrap",
+        }}>
+          <TextField
+            label="Location latitude"
+            id="lat"
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">°</InputAdornment>,
+              inputProps: {min: -90, max: 90}
+            }}
+            type="number"
+            defaultValue={locationData.latitude}
+          />
+          <TextField
+            label="Location longitude"
+            id="long"
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">°</InputAdornment>,
+              inputProps: {min: -180, max: 180}
+            }}
+            type="number"
+            defaultValue={locationData.longitude}
+          />
+        </div>
+      
         <hr/>
-        <img style={{ width: "50px" }} src={locationData.photo} alt=""></img>
-          {/*MISSING IMAGE UPLOAD!!! */}
-        <hr/>
-        <Button 
-          variant="contained"
-          sx={{margin: 1}}
-          color="primary"
-          onClick={()=>{
-            console.log("Clicked save")
-          }}>
-          Save
-        </Button>
-        <Button 
-          onClick={onClose} 
-          variant="contained"
-          sx={{margin: 1}}
-          >
-        Save & Close
-        </Button>
+        <div style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexWrap: "wrap",
+        }}>
+          <img style={{ height: "100px" }} src={locationData.photo} alt=""></img>
+            {/*MISSING IMAGE UPLOAD!!! */}
+        </div>
+        <hr/> 
+        <div style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexWrap: "wrap",
+        }}>
+          <Button 
+            variant="contained"
+            sx={{margin: 1}}
+            color="primary"
+            onClick={()=>{
+              console.log("Saving changes")
+            }}>
+            Save
+          </Button>
+          <Button 
+            onClick={() => {
+              console.log("Saving changes")
+              onClose()}} 
+            color="success"
+            variant="contained"
+            sx={{margin: 1}}
+            >
+          Save & Close
+          </Button>
+          <Button 
+            onClick={() => {
+              console.log("Not saving changes")
+              onClose()}} 
+            variant="contained"
+            color="error"
+            sx={{margin: 1}}
+            >
+          Cancel
+          </Button>
+        </div>
       </div>
-      </Container>
+      </Paper>
     </div>
   );
 };
