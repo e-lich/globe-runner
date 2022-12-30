d27fd47ca4b89bb4faa7ee9ffacb52c4d7cd6b00
Ela <ek53440@fer.hr>
2022-12-30 16:24:57 +0100

    Fixed player and cartographer ids

--- IzvorniKod/backend/database/models.py
+++ IzvorniKod/backend/database/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
 
     __abstract__ = True
 
-    userID = db.Column(db.Integer, primary_key=True, unique=True)
+    userID = db.Column(db.String(32), primary_key=True, unique=True)
     username = db.Column(db.String(32), unique=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(345), unique=True)
@@ -29,7 +29,9 @@ class Cartographer(User):
     document = db.Column(db.Text)
     verified = db.Column(db.Boolean)
 
-    def __init__(self, username, name, email, password, photo, iban, id):
+    def __init__(self, userID, username, name, email, password, photo, iban, id):
+
+        self.userID = userID
         self.username = username
         self.name = name
         self.email = email
@@ -50,8 +52,9 @@ class Player(User):
     eloScore = db.Column(db.Integer)
     playerLocation = db.Column(db.String(100))
 
-    def __init__(self, username, name, email, password, photo):
-
+    def __init__(self, userID, username, name, email, password, photo):
+        
+        self.userID = userID
         self.username = username
         self.name = name
         self.email = email
@@ -90,15 +93,15 @@ class Card(db.Model):
     title = db.Column(db.String(100))
     description = db.Column(db.String(250))
     cardStatus = db.Column(db.Enum("submitted", "unclaimed", "claimed", "verified", name="card_status_type"))
-    authorUserID = db.Column(db.Integer, db.ForeignKey("Players.userID"))
-    cartographerID = db.Column(db.Integer, db.ForeignKey("Cartographers.userID")) 
+    authorUserID = db.Column(db.String(32), db.ForeignKey("Players.userID"))
+    cartographerID = db.Column(db.String(32), db.ForeignKey("Cartographers.userID")) 
 
 # Inventory db model
 class Inventory(db.Model):
     __tablename__ = "Inventories"
 
-    userID = db.Column(db.Integer, db.ForeignKey("Players.userID"), primary_key=True)
-    cardID = db.Column(db.Integer, db.ForeignKey("Cards.cardID"), primary_key=True)
+    userID = db.Column(db.String(32), db.ForeignKey("Players.userID"), primary_key=True)
+    cardID = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"), primary_key=True)
     strength = db.Column(db.Integer)
 
 # Challenge db model
@@ -106,8 +109,8 @@ class Challenge(db.Model):
     __tablename__ = "Challenges"
 
     challengeID = db.Column(db.Integer, primary_key=True, unique=True)
-    challengerUserID = db.Column(db.Integer, db.ForeignKey("Players.userID"))
-    victimUserID = db.Column(db.Integer, db.ForeignKey("Players.userID"))
+    challengerUserID = db.Column(db.String(32), db.ForeignKey("Players.userID"))
+    victimUserID = db.Column(db.String(32), db.ForeignKey("Players.userID"))
     challengeTimestamp = db.Column(db.DateTime)
     challengeStatus = db.Column(db.Enum("pending", "accepted", "rejected", name="challenge_status_type"))
 
@@ -116,14 +119,14 @@ class Fight(db.Model):
     __tablename__ = "Fights"
     
     fightID = db.Column(db.Integer, primary_key=True, unique=True)
-    player1UserID = db.Column(db.Integer, db.ForeignKey("Players.userID"))
-    player2UserID = db.Column(db.Integer, db.ForeignKey("Players.userID"))
-    cardID11 = db.Column(db.Integer, db.ForeignKey("Cards.cardID"))
-    cardID12 = db.Column(db.Integer, db.ForeignKey("Cards.cardID"))
-    cardID13 = db.Column(db.Integer, db.ForeignKey("Cards.cardID"))
-    cardID21 = db.Column(db.Integer, db.ForeignKey("Cards.cardID"))
-    cardID22 = db.Column(db.Integer, db.ForeignKey("Cards.cardID"))
-    cardID23 = db.Column(db.Integer, db.ForeignKey("Cards.cardID"))
+    player1UserID = db.Column(db.String(32), db.ForeignKey("Players.userID"))
+    player2UserID = db.Column(db.String(32), db.ForeignKey("Players.userID"))
+    cardID11 = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"))
+    cardID12 = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"))
+    cardID13 = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"))
+    cardID21 = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"))
+    cardID22 = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"))
+    cardID23 = db.Column(db.BigInteger, db.ForeignKey("Cards.cardID"))
     points1 = db.Column(db.Float)
     points2 = db.Column(db.Float)
     fightTimestamp = db.Column(db.DateTime)
