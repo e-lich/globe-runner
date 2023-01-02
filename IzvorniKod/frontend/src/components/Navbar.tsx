841ae8f4ef91d351e00a32c0e590aa49e9e2bfa7
kazokid <lovro.kovacic7@gmail.com>
2023-01-02 21:25:45 +0100

    added Logout to NavBar

--- IzvorniKod/frontend/src/components/Navbar.tsx
+++ IzvorniKod/frontend/src/components/Navbar.tsx
@@ -1,9 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
+import axios from "axios";
 
 export default function Navbar() {
   const navigate = useNavigate();
   var user = JSON.parse(localStorage.getItem("user") || "{}");
+
+  function handleLogout() {
+    const logout = async () => {
+      try {
+        const response = await axios.post("/logout");
+
+        if (response.status !== 200) {
+          console.log("Something went wrong while logging out");
+        }
+      } catch (error: any) {
+        console.log(error);
+      }
+    };
+
+    logout();
+    localStorage.removeItem("user");
+
+    navigate("/login");
+  }
 
   if (user)
     return (
@@ -80,6 +100,8 @@ export default function Navbar() {
                 Cartographer Requests
               </Dropdown.Item>
             ) : null}
+
+            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
 
--- IzvorniKod/frontend/src/pages/user/UserProfile.tsx
+++ IzvorniKod/frontend/src/pages/user/UserProfile.tsx
@@ -8,154 +8,46 @@ import UserProfileCard from "../../components/UserProfile/UserProfileCard";
 import UserStatsCard from "../../components/UserProfile/UserStatsCard";
 
 export default function UserProfile() {
-  const [userData, setData] = useState({
-    username: "username",
-    password: "",
-    passwordConfirmation: "",
-  });
-
-  // TODO Handle password changes
-
   const navigate = useNavigate();
-  const [submitDisabled, setSubmitDisabled] = useState(true);
-  const [authMode, setAuthMode] = useState("basic");
   const [user, setUser] = useState(localStorage.getItem("user"));
 
-  function handleLogout() {
-    const logout = async () => {
-      try {
-        const response = await axios.post("/logout");
-
-        if (response.status !== 200) {
-          console.log("Something went wrong while logging out");
-        }
-      } catch (error: any) {
-        console.log(error);
-      }
-    };
-
-    logout();
-    localStorage.removeItem("user");
-
-    navigate("/login");
-  }
-
-  const handleChanges = (e: any) => {
-    setData({ ...userData, [e.target.name]: e.target.value });
-  };
-
-  const changeAuthMode = () => {
-    setAuthMode(authMode === "basic" ? "change" : "basic");
-  };
+  // todo ovo ne treba biti ovdje nego u navbaru
 
   useEffect(() => {
     let userFromLocalStorage = localStorage.getItem("user");
 
     if (userFromLocalStorage === null) navigate("/login");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    if (
-      userData.password !== "" &&
-      userData.password.length >= 8 &&
-      userData.password === userData.passwordConfirmation
-    )
-      setSubmitDisabled(false);
-    else setSubmitDisabled(true);
-  }, [userData.password, userData.passwordConfirmation]);
-
-  if (authMode === "basic") {
-    return (
-      <>
-        <Navbar />
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <Typography variant="h4" sx={{ m: 2 }}>
-              My profile
-            </Typography>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <UserProfileCard
-                  user={JSON.parse(user!)}
-                  numberOfLocations={12}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <UserStatsCard />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h4" sx={{ m: 2 }}>
-              Inventory
-            </Typography>
-            <UserInventory />
-          </Grid>
-        </Grid>
-      </>
-    );
-  }
 
   return (
     <>
       <Navbar />
-      <div className="Auth-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Profile information</h3>
-            <div className="form-group mt-3">
-              <label>Email address</label>
-              <input
-                type="email"
-                className="form-control mt-1"
-                placeholder={user ? JSON.parse(user).email : "email"}
-                disabled={true}
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography variant="h4" sx={{ m: 2 }}>
+            My profile
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <UserProfileCard
+                user={JSON.parse(user!)}
+                numberOfLocations={12}
               />
-            </div>
-            <div className="form-group mt-3">
-              <label>New Password</label>
-              <input
-                type="password"
-                name="password"
-                value={userData.password}
-                className="form-control mt-1"
-                placeholder="Password"
-                onChange={(e) => handleChanges(e)}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>New Password Confirmation</label>
-              <input
-                type="password"
-                name="passwordConfirmation"
-                value={userData.passwordConfirmation}
-                className="form-control mt-1"
-                placeholder="Password Confirmation"
-                onChange={(e) => handleChanges(e)}
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={(e) => {
-                  /*TODO*/
-                }}
-                disabled={submitDisabled}
-              >
-                Submit
-              </button>
-              <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={changeAuthMode}
-              >
-                Cancel Password Change
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+            </Grid>
+            <Grid item xs={12}>
+              <UserStatsCard />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h4" sx={{ m: 2 }}>
+            Inventory
+          </Typography>
+          <UserInventory />
+        </Grid>
+      </Grid>
     </>
   );
 }
