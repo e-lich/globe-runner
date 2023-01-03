b1aa6817da3d73864e87aef7f2202170f6933127
Ela <ek53440@fer.hr>
2023-01-03 18:09:24 +0100

    Fixes

--- IzvorniKod/backend/views/user_views/get_users.py
+++ IzvorniKod/backend/views/user_views/get_users.py
@@ -11,7 +11,8 @@ def formattedReturn(users):
             "email": user.email,
             "confirmed": user.confirmed,
             "userType": user.__class__.__name__,
-            "profilePhoto": user.profilePhoto
+            "profilePhoto": user.profilePhoto,
+            "banned": user.banned
         })
 
     if len(users_arr) == 0:
