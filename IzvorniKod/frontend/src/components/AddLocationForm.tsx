0ed2d704fadccc292203089d6fe6122129155acc
Vladimir Pezo <vladimir.pezo@fer.hr>
2023-01-05 22:32:16 +0100

    replaced the form, button does not allow submit. photo is the problem

--- IzvorniKod/frontend/src/components/AddLocationForm.tsx
+++ IzvorniKod/frontend/src/components/AddLocationForm.tsx
@@ -1,7 +1,15 @@
 import Box from "@mui/material/Box";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
+import { Alert, Button, Fab, Typography } from "@mui/material";
+import axios from "axios";
+import * as Yup from "yup";
+import ImageSearchIcon from "@mui/icons-material/ImageSearch";
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import { useNavigate } from "react-router-dom";
+import { error } from "console";
+import { useState } from "react";
+
 
 export default function AddLocationForm({
   latitude,
@@ -19,57 +27,8 @@ export default function AddLocationForm({
 
   userLongitude: Number | undefined;
 }) {
-  //  MAKE POST REQUEST!
-  // const [title, setTitle] = useState("");
-  // const [description, setDescription] = useState("");
-  /* const [file, setFile] = useState<Blob>();
   let [error, setError] = useState<Array<String>>([]);
-  let [submitDisabled, setSubmitDisabled] = useState(true);
-
-  function locationPhotoChange(e: React.ChangeEvent<HTMLInputElement>): void {
-    setError((prev) =>
-      prev.filter((e) => e !== "Image must be less than 1MB!")
-    );
-    if (
-      e.target.files &&
-      e.target.files[0] &&
-      e.target.files[0].size < 1000000 &&
-      e.target.files[0].type === "image/jpeg"
-    ) {
-      setFile(e.target.files[0]);
-    } else {
-      setError((previousValue) => [
-        ...previousValue,
-        "Image must be less than 1MB!",
-      ]);
-    }
-  }
-
-  function handleSubmit() {
-    if (!file) {
-      setError((previousValue) => [
-        ...previousValue,
-        "You must upload a location picture!",
-      ]);
-      return;
-    }
-
-    let formData = new FormData();
-
-    formData.append("title", title); //append the values with key, value pair
-    formData.append("description", description);
-    formData.append("lat", lat);
-    formData.append("long", long);
-
-    const config = {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    };
-    //axios
-    console.log(formData)
-    return;
-  }*/
+  const navigate = useNavigate();
 
   const setUserLocation = async () => {
     console.log("setting form location to the user location!");
@@ -77,65 +36,252 @@ export default function AddLocationForm({
     setLongitude(userLongitude);
   };
 
+  const initialValues = {
+    title: "",
+    description: "",
+    latitude: latitude,
+    longitude: longitude,
+    locationPhoto: undefined,
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("Required"),
+    description: Yup.string().min(20).required("Required"),
+    latitude: Yup.number().required("Required"),
+    longitude: Yup.number().required("Required"),
+    LocationPhoto: Yup.mixed()
+      .test("photoSize", "Photo too large", photoSizeCheck)
+      .required("Required"),
+  });
+
+  function photoSizeCheck(locationPhoto?: Blob): boolean {
+    if (locationPhoto === undefined) {
+      console.log("photo undefined");
+      return false;
+    }
+    console.log("photo size: " + locationPhoto.size);
+    return locationPhoto.size <= 1000000;
+  }
+
+  const handleSubmit = async (values: any) => {
+    console.log("saving location");
+
+    setError([]);
+
+    let formData = new FormData();
+
+    formData.append("title", values.title);
+    formData.append("description", values.description);
+    formData.append("latitude", values.latitude);
+    formData.append("longitude", values.longitude);
+    formData.append("locationPhoto", values.locationPhoto);
+
+    axios
+      .post("/locations/add/submitted", formData)
+      .then((res) => {
+        console.log(res);
+        if (res.data.success != true) {
+          setError(res.data);
+        } else {
+          navigate("/addLocation");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return;
+  }
+
   return (
-    <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-      <div>
-        {/*need to center this*/}
-        <TextField
-          label="Card title"
-          id="title"
-          sx={{ m: 1, width: "25ch" }}
-          type="text"
-        />
-        <TextField
-          id="description"
-          label="Description"
-          placeholder="Please describe the location in couple of sentences"
-          multiline
-          sx={{ m: 1, width: "25ch" }}
-        />
-        <hr />
-        <TextField
-          label="Location latitude"
-          disabled={true}
-          value={latitude}
-          id="lat"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">°</InputAdornment>,
-            inputProps: { min: -90, max: 90 },
-          }}
-          type="number"
-        />
-        <TextField
-          label="Location longitude"
-          disabled={true}
-          value={longitude}
-          id="long"
-          sx={{ m: 1, width: "25ch" }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">°</InputAdornment>,
-            inputProps: { min: -180, max: 180 },
-          }}
-          type="number"
-          helperText="You can click on the map or use your location!"
-        />
-        <Button variant="outlined" color="primary" onClick={setUserLocation}>
-          Use my location
-        </Button>
-        <hr />
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={() => {
-            console.log("Clicked submit");
-          }}
-        >
-          Submit
-        </Button>
-        <hr />
-      </div>
-      <div id="mapid"></div>
+    <>
+    {error.length > 0 ? 
+          error.map((err, key) => (
+            <Alert key={key} severity="error" sx={{ mb: 1 }}>
+              <strong>Error: </strong> {err}
+            </Alert>
+    )) : ""}
+        
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {(props) => (
+          <Form>
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Field
+                  as={TextField}
+                  label="Card title"
+                  name="title"
+                  placeholder="Edit location title"
+                  fullWidth
+                  required
+                  error={props.errors.title && props.touched.title}
+                  helperText={<ErrorMessage name="title" />}
+                  sx={{ m: 1, width: '25ch' }}
+                  />
+                <Field
+                  as={TextField}
+                  label="Description"
+                  name="description"
+                  placeholder="Edit location description"
+                  fullWidth
+                  multiline
+                  required
+                  error={props.errors.description && props.touched.description}
+                  helperText={<ErrorMessage name="description" />}
+                  sx={{ m: 1, width: '25ch' }}
+
+                />
+            </Box>
+            <hr />
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Field
+                as={TextField}
+                label="Latitude"
+                name="latitude"
+                placeholder="Edit location latitude"
+                fullWidth
+                value = {latitude}
+                required
+                error={props.errors.latitude && props.touched.latitude}
+                helperText={<ErrorMessage name="latitude" />}
+                sx={{ m: 1, width: '25ch' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">°</InputAdornment>),
+                    inputProps: { min: -90, max: 90},
+                }}
+                type="number"
+                />
+              <Field
+                as={TextField}
+                label="Longitude"
+                name="longitude"
+                placeholder="Edit location longitude"
+                fullWidth
+                value = {longitude}
+                required
+                error={props.errors.longitude && props.touched.longitude}
+                helperText={<ErrorMessage name="longitude" />}
+                sx={{ m: 1, width: '25ch' }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">°</InputAdornment>),
+                    inputProps: { min: -180, max: 180},
+                }}
+                type="number"
+                />
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ mb: 1 }}
+                  textAlign="center"
+                >
+                  Click on the map or use the button below!
+                </Typography>
+                <Button variant="outlined" color="primary" onClick={setUserLocation}>
+                  Use my location
+                </Button>
+            </Box>
+
+            <hr />
+            <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            >
+              <input
+                hidden
+                id="locationPhoto"
+                name="locationPhoto"
+                type="file"
+                accept="image/*"
+                onChange={(event) => {
+                  props.setFieldValue(
+                    "locationPhoto",
+                    event.currentTarget.files![0]
+                  );
+                  console.log("added pic  " + props.values.locationPhoto);
+                }}
+              />
+                <label htmlFor="locationPhoto">
+                  <Typography 
+                    noWrap sx={{ mb: 1 }}
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    Location picture
+                  </Typography>
+                  <Fab component="span" sx={{ mb: 3 }}>
+                    <ImageSearchIcon />
+                  </Fab>
+                </label>
+
+                {props.values.locationPhoto !== undefined &&(
+                    <Box
+                      component="img"
+                      alt="location pic"
+                      src={URL.createObjectURL(props.values.locationPhoto)}
+                      sx={{
+                        width: 0.3,
+                        border: 3,
+                        borderRadius: "2%",
+                      }}
+                    />
+                  )}
+
+            </Box>
+            <hr />
+            <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            >
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                disabled={!props.isValid}
+                sx={{margin: 1}}
+              >
+                Submit
+              </Button>
+            </Box>
+          </Form>)}
+      </Formik>
     </Box>
+    </>
+
+   
   );
 }
