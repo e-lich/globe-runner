8e99cd0d658c3adc6f1c7e6b0b124aef50da5b5e
kazokid <lovro.kovacic7@gmail.com>
2023-01-05 23:07:02 +0100

    connected inventory to backend

--- IzvorniKod/frontend/src/components/UserProfile/UserInventory.tsx
+++ IzvorniKod/frontend/src/components/UserProfile/UserInventory.tsx
@@ -1,53 +1,20 @@
 import { Grid, List, Paper } from "@mui/material";
-import { Card } from "react-bootstrap";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import LocationCard from "../LocationCard";
 
-const InventoryCards = [
-  {
-    title: "Card_01",
-    lat: 15.2132,
-    lng: 45.154425,
-    image: "../images/card_photo.jpg",
-  },
-  {
-    title: "Card_02",
-    lat: 15.214,
-    lng: 45.1545,
-    image: "../images/card_photo.jpg",
-  },
-  {
-    title: "Card_03",
-    lat: 15.215,
-    lng: 45.15455,
-    image: "../images/card_photo.jpg",
-  },
-  {
-    title: "Card_04",
-    lat: 15.2155,
-    lng: 45.1546,
-    image: "../images/card_photo.jpg",
-  },
-  {
-    title: "Card_03",
-    lat: 15.215,
-    lng: 45.15455,
-    image: "../images/card_photo.jpg",
-  },
-  {
-    title: "Card_04",
-    lat: 15.2155,
-    lng: 45.1546,
-    image: "../images/card_photo.jpg",
-  },
-  {
-    title: "Card_03",
-    lat: 15.215,
-    lng: 45.15455,
-    image: "../images/card_photo.jpg",
-  },
-];
-
 export default function UserInventory() {
+  const [InventoryCards, setInventoryCards] = useState([]);
+
+  useEffect(() => {
+    const getInventoryCards = async () => {
+      const response = await axios.get("locations/owned");
+      setInventoryCards(response.data);
+    };
+
+    getInventoryCards();
+  }, []);
+
   return (
     <>
       <Paper
