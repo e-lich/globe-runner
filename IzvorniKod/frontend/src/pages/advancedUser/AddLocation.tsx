855c5bee96a351e201fb95e0b0c8705046011ee2
Vladimir Pezo <vladimir.pezo@fer.hr>
2023-01-05 22:32:16 +0100

    replaced the form, button does not allow submit. photo is the problem

--- IzvorniKod/frontend/src/pages/advancedUser/AddLocation.tsx
+++ IzvorniKod/frontend/src/pages/advancedUser/AddLocation.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AddLocationMap from "../../components/AddLocationMap";
 import AddLocationForm from "../../components/AddLocationForm";
+import { Grid } from "@mui/material";
 
 export default function AddLocation() {
   const navigate = useNavigate();
@@ -28,14 +29,17 @@ export default function AddLocation() {
   return (
     <>
       <Navbar />
-      <AddLocationMap
+      <Grid container>
+        <Grid item xs={12} sm={7} style={{ height: "70vh" }}>
+        <AddLocationMap
         setLatitude={setLatitude}
         setLongitude={setLongitude}
         setUserLatitude={setUserLatitude}
         setUserLongitude={setUserLongitude}
       />
-      <div className="align-items-center Auth-container">
-        <form className="Auth-form">
+        </Grid>
+        <Grid item xs={12} sm={5}>
+        <div className="align-items-center Auth-container">
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Add Locations</h3>
             <AddLocationForm
@@ -47,8 +51,9 @@ export default function AddLocation() {
               userLongitude={userLongitude}
             />
           </div>
-        </form>
       </div>
+        </Grid>
+      </Grid>
     </>
   );
 }
