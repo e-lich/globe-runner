d8ceaa7710bfd5395f3203e3b614afe60e9e118e
Lovro Raguž <lr53603@fer.hr>
2023-01-06 16:18:06 +0100

    User stats

--- IzvorniKod/backend/views/statistics_views/statistics.py
+++ IzvorniKod/backend/views/statistics_views/statistics.py
@@ -3,7 +3,7 @@ from flask import request, jsonify, session, redirect
 from backend.database.models import Player, Fight
 from geopy import distance
 import json
-from sqlalchemy import inspect
+from sqlalchemy import or_, and_
 
 @app.route('/stats/leaderboard', methods=['GET'])
 def get_leaderboard_stats():
@@ -29,4 +29,40 @@ def get_collected_locations_stats():
 
 @app.route('/users/stats', methods=['GET'])
 def get_user_stats():
-    return ['Request not implemented']
+    userID = session['userID']
+
+    if userID == None:
+        return ["User not logged in."]
+
+    if session['userType'] != "Player" and session['userType'] != "advancedPlayer":
+        return ["Only players can get stats"]
+
+    count = 1
+    players_ordered = db.session.query(Player).order_by(Player.eloScore.desc())
+
+    for player in players_ordered:
+        if player.userID == session['userID']:
+            break
+
+        count += 1
+
+    wonClause = or_(and_(Fight.player1UserID==userID, Fight.points1 > Fight.points2),\
+                    and_(Fight.player2UserID==userID, Fight.points2 > Fight.points1))
+
+    lostClause = or_(and_(Fight.player1UserID==userID, Fight.points2 > Fight.points1),\
+                     and_(Fight.player2UserID==userID, Fight.points1 > Fight.points2))
+
+    fightsWon = db.session.query(Fight).filter(and_(Fight.points1!=0, Fight.points2!=0))\
+        .filter(wonClause).count()
+
+    fightsLost = db.session.query(Fight).filter(and_(Fight.points1!=0, Fight.points2!=0))\
+        .filter(lostClause).count()
+
+    fightsNum = db.session.query(Fight).filter(and_(Fight.points1!=0, Fight.points2!=0)).count()
+
+    return jsonify({
+        "fightsNum": fightsNum,
+        "fightsWon": fightsWon,
+        "fightsLost": fightsLost,
+        "rank": count
+    })
