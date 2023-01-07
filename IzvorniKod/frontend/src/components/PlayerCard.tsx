f7b53959e78d08ad1033aaafdeae482a75068c61
kazokid <lovro.kovacic7@gmail.com>
2023-01-07 20:37:15 +0100

    started working on vieewng another players profile

--- IzvorniKod/frontend/src/components/PlayerCard.tsx
+++ IzvorniKod/frontend/src/components/PlayerCard.tsx
@@ -2,8 +2,8 @@ import { ListItem, ListItemAvatar, Avatar } from "@mui/material";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
 import ChallengeIcon from "./ChallengeIcon";
-import ViewpProfilePopup from "./ViewProfilePopup";
 import ViewProfilePopup from "./ViewProfilePopup";
+import ViewPlayerProfileDialog from "./ViewPlayerProfileDialog";
 
 export default function PlayerCard(props: any) {
   const placeholder = require("../images/profile_picture.jpg");
@@ -17,7 +17,7 @@ export default function PlayerCard(props: any) {
         </ListItemAvatar>
         <div className="player-buttons">
           <Button
-            variant="text"  
+            variant="text"
             color="primary"
             onClick={() => {
               setIsViewOpen(true);
@@ -29,14 +29,13 @@ export default function PlayerCard(props: any) {
         </div>
       </ListItem>
 
-      <ViewProfilePopup
+      <ViewPlayerProfileDialog
         open={isViewOpen}
         onClose={() => {
           setIsViewOpen(false);
         }}
         player={props.closestPlayer}
-        />
+      />
     </>
-    
   );
 }
