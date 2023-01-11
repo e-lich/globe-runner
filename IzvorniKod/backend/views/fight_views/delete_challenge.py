a74384f9fd1060394852af776c424dcf6ca67343
Ela <ek53440@fer.hr>
2023-01-11 23:03:24 +0100

    Delete challenge fix

---
+++ IzvorniKod/backend/views/fight_views/delete_challenge.py
@@ -0,0 +1,21 @@
+from backend import db, app
+from flask import request, jsonify, session
+from backend.database.models import Challenge
+
+@app.route('/fight/challenges/delete', methods=['POST', 'GET'])
+def delete_challenge():
+    if "userID" not in session:
+        return(['User not logged in'])
+    
+    user_type = session["userType"]
+    currentUserID = session["userID"]
+
+    if user_type != "Player":
+        return ["Only players can delete challenges"]
+
+    if request.method == 'POST':
+        db.session.query(Challenge).filter_by(challengerUserID=currentUserID).delete()
+        db.session.commit()
+        return jsonify(success=True)
+    else:
+        return ["Invalid request method"]
