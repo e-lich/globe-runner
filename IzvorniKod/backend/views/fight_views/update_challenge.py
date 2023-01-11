2a8bffc6b10490416d0960c1b6defce7f785a292
Lovro Raguž <lr53603@fer.hr>
2023-01-11 21:56:01 +0100

    Add POST /fight/challenges/delete

--- IzvorniKod/backend/views/fight_views/update_challenge.py
+++ IzvorniKod/backend/views/fight_views/update_challenge.py
@@ -63,3 +63,23 @@ def respond_to_challenge(challengeID):
     
     else:
         return ["Invalid request method"]
+
+@app.route('/fight/challenges/delete', methods=['POST', 'GET'])
+def delete_challenge():
+    if "userID" not in session:
+        return(['User not logged in'])
+    
+    user_type = session["userType"]
+    currentUserID = session["userID"]
+
+    if user_type != "Player":
+        return ["Only players can delete challenges"]
+
+    if request.method == 'POST':
+        db.session.query(Challenge).filter_by(challengerUserID=currentUserID).delete()
+        db.session.commit()
+        return jsonify(success=True)
+    else:
+        return ["Invalid request method"]
+
+  
