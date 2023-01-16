d5e9b46c9608a9fd429c7a87a0b555fd1055f318
kazokid <lovro.kovacic7@gmail.com>
2023-01-16 23:58:48 +0100

    fixed popup in fights

--- IzvorniKod/frontend/src/components/BrokenCards.tsx
+++ IzvorniKod/frontend/src/components/BrokenCards.tsx
@@ -40,6 +40,7 @@ export default function BrokenCards() {
                     cardOnClick={() => {
                       console.log("Select this card.");
                     }}
+                    hasPopup={true}
                   />
                 </Grid>
               ))}
--- IzvorniKod/frontend/src/pages/Fights.tsx
+++ IzvorniKod/frontend/src/pages/Fights.tsx
@@ -136,6 +136,7 @@ export default function Fights() {
                         pickThisCard(InventoryCard);
                       }
                     }}
+                    hasPopup={false}
                   />
                 </Grid>
               ))}
