bf192cfdab48f19128018b23166ff3d6faa09b18
Ela <ek53440@fer.hr>
2023-02-12 20:48:22 +0100

    fix api

--- IzvorniKod/frontend/src/index.tsx
+++ IzvorniKod/frontend/src/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-axios.defaults.baseURL = "https://globerunner.games/api";
+axios.defaults.baseURL = "http://127.0.0.1:5000";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
